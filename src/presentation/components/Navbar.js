9187a3994ce91929371627da1a3a4841857ba323
Tehillah Kangamba <>
2021-09-09 22:16:47 -0500

    started mandi website

---
+++ src/presentation/components/Navbar.js
@@ -0,0 +1,34 @@
+import Logo from '../assets/logo.png'
+import Home from "../pages/Home";
+const Navbar = () => {
+    return(
+        <header>
+
+            <nav className={"flex nav-jc-sb"}>
+                <a className={"nav-logo"} href={Home}>
+                    <img src={Logo} alt={"Company logo"} />
+                </a>
+                <ul className={"nav-jc-c"}>
+                    <li>
+                        <a href={""}>Home</a>
+                    </li>
+
+                    <li>
+                        <a href={""}>Books</a>
+                    </li>
+
+                    <li>
+                        <a href={""}>Our Services</a>
+                    </li>
+
+                    <li>
+                        <a href={""}>Contact us</a>
+                    </li>
+                </ul>
+
+            </nav>
+
+        </header>
+    )
+}
+export default Navbar;
