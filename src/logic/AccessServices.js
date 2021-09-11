391176746873f6a943421a694ec78030b72394f5
Tehillah Kangamba <>
2021-09-11 18:37:13 -0500

    used local images for services

--- src/logic/AccessServices.js
+++ src/logic/AccessServices.js
@@ -1,20 +1,22 @@
-
+import PublishingImage from '../presentation/assets/publishing-services.jpg'
+import EditingImage from '../presentation/assets/editing-services.jpg'
+import MentoringImage from '../presentation/assets/mentoring-service.jpg'
 const Services=[
     {
         id:1,
-        imageUrl: 'https://images.pexels.com/photos/159778/books-reading-series-narnia-159778.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        imageUrl: PublishingImage,
         name:'Publishing',
         description:'We print and guide self publishers as self publishing has become more popular'
     },
     {
         id:2,
-        imageUrl: 'https://images.pexels.com/photos/6579275/pexels-photo-6579275.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        imageUrl: EditingImage,
         name:'Editing',
         description:`Copy editing,developmental editing,proof reading, manuscript critique`
     },
     {
         id:3,
-        imageUrl:'https://images.pexels.com/photos/935949/pexels-photo-935949.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        imageUrl:MentoringImage,
         name:'Mentoring',
         description:`We mentor not only writers but public speakers and editors`
     }
