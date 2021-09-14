af321f1b4e43fb014315a8db972f658d47f243e5
Tehillah Kangamba <>
2021-09-13 23:21:38 -0500

    made code prettier

--- src/presentation/components/BookList.js
+++ src/presentation/components/BookList.js
@@ -1,60 +1,55 @@
-import {getBooks} from "../../logic/AccessBooks";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAmazon,faEtsy } from '@fortawesome/free-brands-svg-icons'
-
+import { getBooks } from "../../logic/AccessBooks";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAmazon, faEtsy } from "@fortawesome/free-brands-svg-icons";
 
 function BookCard(props) {
-    const book=props.book;
-    const cover =book.coverPic;
+    const book = props.book;
+    const cover = book.coverPic;
 
     const openBookPage = (link) => {
-        window.open(link)
-    }
+        window.open(link);
+    };
 
-    return(
+    return (
         <div className={"menu-card"}>
             <div className={"card-text"}>
-               <h2>{book.name}</h2>
+                <h2>{book.name}</h2>
                 <p>{book.description}</p>
                 <div className={"buttons"}>
-
-                    <button className={"blue-button"} onClick={
-                        ()=>{
-                            openBookPage(book.amazonLink)
-                        }
-
-                    }>
-                        <FontAwesomeIcon icon={faAmazon}/>
+                    <button
+                        className={"blue-button"}
+                        onClick={() => {
+                            openBookPage(book.amazonLink);
+                        }}
+                    >
+                        <FontAwesomeIcon icon={faAmazon} />
                         Amazon
                     </button>
-                    <button className={"blue-button"}
-                       onClick={ ()=>{
-                           openBookPage(book.etsyLink)
-                        }
-                       }
+                    <button
+                        className={"blue-button"}
+                        onClick={() => {
+                            openBookPage(book.etsyLink);
+                        }}
                     >
-                        <FontAwesomeIcon icon={faEtsy}/>
-                         Etsy
+                        <FontAwesomeIcon icon={faEtsy} />
+                        Etsy
                     </button>
                 </div>
             </div>
-            <img src={cover} alt={''} />
+            <img src={cover} alt={""} />
         </div>
-    )
+    );
 }
 const BookList = () => {
-    console.log(getBooks())
-    const  books = getBooks();
-    return(
+    console.log(getBooks());
+    const books = getBooks();
+    return (
         <div>
-            {
-                books.map(
-                    (book)=><BookCard key={book.id} book={book} />
-                )
-            }
+            {books.map((book) => (
+                <BookCard key={book.id} book={book} />
+            ))}
         </div>
-    )
-}
-
+    );
+};
 
 export default BookList;
