fb8a24b64cbd7e781ec65d1f458899cf1b172624
Tehillah Kangamba <>
2021-11-10 04:15:19 -0600

    made image fluid

--- src/presentation/components/ServicesBanner.js
+++ src/presentation/components/ServicesBanner.js
@@ -18,7 +18,7 @@ function Card(props) {
                     </Link>
                 </div>
             </div>
-            <img src={service.menuImg} alt={""} style={{width:"15rem",height:"25rem",objectFit:"cover"}} />
+            <img className="img-fluid" src={service.menuImg} alt={""} style={{width:"15rem",height:"25rem",objectFit:"cover"}} />
         </div>
     );
 }
